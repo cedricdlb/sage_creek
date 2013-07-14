9071b62df16c21b5ba4a786160eee52d71b23ff4
Cedric de La Beaujardiere <cdlb.git@gmail.com>
2013-07-14 03:23:41 -0700

    result of running: rails new .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SageCreek::Application.config.secret_key_base = '7cbab08197a27fd6c01b3a598cd4c53809b863a7985778d79c50cc8bb0f2b898e50961ad3773e05ad248483b7b1a98e608cbb7ecf04fa4bdca57805f88ea6a6a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SageCreek::Application.config.session_store :cookie_store, key: '_sage_creek_session'
